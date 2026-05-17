91b3c8bc41150349dba77dee596afbae35c5bb80
Jonathan Cornejo <jonacornejocl@gmail.com>
2026-05-17 09:58:21 -0400

    modelo

--- ms_catalogo/src/main/java/cl/tipum/blinblineo/ms_catalogo/model/Producto.java
+++ ms_catalogo/src/main/java/cl/tipum/blinblineo/ms_catalogo/model/Producto.java
@@ -1,5 +1,41 @@
 package cl.tipum.blinblineo.ms_catalogo.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "producto")
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String sku;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+
+    @Column(nullable = false)
+    private Integer precio;
+
+    @Column(nullable = false, length = 20)
+    private String estado;
 
 }
