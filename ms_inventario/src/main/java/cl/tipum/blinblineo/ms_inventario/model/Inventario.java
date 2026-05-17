0962f84bdf34251673bdf512e919fd3d7ae3875b
Jonathan Cornejo <jonacornejocl@gmail.com>
2026-05-17 15:45:06 -0400

    repositorio

---
+++ ms_inventario/src/main/java/cl/tipum/blinblineo/ms_inventario/model/Inventario.java
@@ -0,0 +1,32 @@
+package cl.tipum.blinblineo.ms_inventario.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "inventario")
+public class Inventario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String sku;
+
+    @Column(nullable = false)
+    private Integer cantidad;
+
+}
