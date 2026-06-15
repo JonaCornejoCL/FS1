49bfa2bdb391c8011b7d94de3fdef293abe54ecf
Jonathan Cornejo <jonacornejocl@gmail.com>
2026-06-14 23:38:49 -0400

    Fix: Corrección de nombres en Eureka y estabilización del API Gateway

--- ms_eureka_server/src/main/java/cl/tipum/blinblineo/ms_eureka_server/MsEurekaServerApplication.java
+++ ms_eureka_server/src/main/java/cl/tipum/blinblineo/ms_eureka_server/MsEurekaServerApplication.java
@@ -2,12 +2,14 @@ package cl.tipum.blinblineo.ms_eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsEurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsEurekaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsEurekaServerApplication.class, args);
+    }
 
 }
